b0cdcfca25e57cb8dd75dfaa06ebfa718c6ae555
Luiz Felipe Guerrero <felipe.russi.guerrero@gmail.com>
2020-03-28 15:13:34 -0300

    remove unused constants

--- app/src/main/java/com/comida/indice/syncyourlights/helper/Constants.kt
+++ app/src/main/java/com/comida/indice/syncyourlights/helper/Constants.kt
@@ -2,7 +2,5 @@ package com.comida.indice.syncyourlights.helper
 
 object Constants {
     const val CLIENT_ID = "07937a18dfbb4170853306737eed7d12"
-//    const val REDIRECT_URI = "http://localhost:8888/callback"
     const val REDIRECT_URI = "synclightsapp://callback"
-    const val APP_TAG = "SyncYourLights"
 }
